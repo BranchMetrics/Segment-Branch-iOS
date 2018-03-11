bad345aa7a26c397a618fcb70f1496f08e1c1de9
Edward Smith <esmith@branch.io>
2018-03-11 12:14:10 -0700

    Added 'Send Branch Event Only' for testing.

--- Examples/Fortune/Fortune/EventViewController.swift
+++ Examples/Fortune/Fortune/EventViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Analytics
+import Branch
 
 class EventViewController: UIViewController {
     @IBOutlet weak var segmentActionButton: UIButton!
@@ -38,7 +39,8 @@ class EventViewController: UIViewController {
             "Set Alias",
             "Reset Identity",
             "Track Event",
-            "Track Screen"
+            "Track Screen",
+            "Send Branch Event"
         ]
         self.menuView = APDropDownMenu.init(
             view: self.segmentActionButton,
@@ -83,6 +85,9 @@ class EventViewController: UIViewController {
                     let name = "Segment Screen Test"
                     self.eventBodyTextView.text = "Screen: \(name)"
                     Analytics.screen(name)
+                case 5:
+                    self.eventNameLabel.text = "Send Branch Event"
+                    self.sendBranchEvent()
                 default:
                     break
                 }
@@ -129,4 +134,64 @@ class EventViewController: UIViewController {
         }
     }
 
+    func sendBranchEvent() {
+        let buo = BranchUniversalObject.init()
+
+        buo.contentMetadata.contentSchema    = .commerceProduct;
+        buo.contentMetadata.quantity         = 2;
+        buo.contentMetadata.price            = 23.20
+        buo.contentMetadata.currency         = .USD;
+        buo.contentMetadata.sku              = "1994320302";
+        buo.contentMetadata.productName      = "my_product_name1";
+        buo.contentMetadata.productBrand     = "my_prod_Brand1";
+        buo.contentMetadata.productCategory  = .babyToddler;
+        buo.contentMetadata.productVariant   = "3T";
+        buo.contentMetadata.condition        = .fair;
+
+        buo.contentMetadata.ratingAverage    = 5;
+        buo.contentMetadata.ratingCount      = 5;
+        buo.contentMetadata.ratingMax        = 7;
+        buo.contentMetadata.rating           = 6;
+        buo.contentMetadata.addressStreet    = "Street_name1";
+        buo.contentMetadata.addressCity      = "city1";
+        buo.contentMetadata.addressRegion    = "Region1";
+        buo.contentMetadata.addressCountry   = "Country1";
+        buo.contentMetadata.addressPostalCode = "postal_code";
+        buo.contentMetadata.latitude         = 12.07;
+        buo.contentMetadata.longitude        = -97.5;
+        buo.contentMetadata.imageCaptions    = [ "my_img_caption1", "my_img_caption_2"]
+        buo.contentMetadata.customMetadata   = [
+            "Custom_Content_metadata_key1": "Custom_Content_metadata_val1",
+            "Custom_Content_metadata_key2": "Custom_Content_metadata_val2",
+            "~campaign": "Parul's campaign"
+        ]
+        buo.title                       = "Parul Title";
+        buo.canonicalIdentifier         = "item/12345";
+        buo.canonicalUrl                = "https://branch.io/deepviews";
+        buo.keywords                    = ["My_Keyword1", "My_Keyword2"];
+        buo.contentDescription          = "my_product_description1";
+        buo.imageUrl                    = "https://test_img_url";
+        buo.expirationDate              = Date.init(timeIntervalSinceNow: 24*60*60)
+        buo.publiclyIndex               = false
+        buo.locallyIndex                = true
+        buo.creationDate                = Date.init()
+
+        let event = BranchEvent.standardEvent(.purchase)
+        event.transactionID   = "12344555"
+        event.currency        = .USD;
+        event.revenue         = 1.5
+        event.shipping        = 10.2
+        event.tax             = 12.3
+        event.coupon          = "test_coupon"
+        event.affiliation     = "test_affiliation";
+        event.eventDescription = "Event _description";
+        event.customData      = [
+            "Custom_Event_Property_Key1": "Custom_Event_Property_val1",
+            "Custom_Event_Property_Key2": "Custom_Event_Property_val2"
+        ]
+        event.contentItems = [ buo ]
+        event.logEvent()
+        let dictionary = event.dictionary()
+        self.eventBodyTextView.text = dictionary.description
+    }
 }
